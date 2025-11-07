c27420caf5e4a94c86aaa889ab484e666ad43667
Muhammad Daffa Alwafi <160733737+i7i7s@users.noreply.github.com>
2025-11-07 22:33:14 +0700

    Use Railway environment variables

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // 1. Impor library
 const express = require('express');
 const mysql = require('mysql2/promise');
-const cors = require('cors'); // <-- WAJIB ADA untuk hosting
+const cors = require('cors'); 
 const app = express();
 
 // 2. Aktifkan CORS
 app.use(cors());
 
 // 3. Hubungkan ke Database Railway (Otomatis)
-// Variabel .env ini akan otomatis diisi oleh Railway
+// Menggunakan hostname INTERNAL yang paling andal dan port default 3306
 const dbPool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  host: 'mysql.railway.internal', // <-- HOSTNAME INTERNAL PALING ANDAL
+  port: 3306, // <-- PORT INTERNAL
+  user: 'root',
+  password: 'SKFYICRESSqYSlMWlNYMGtAZrKMCHiVc', // Password Anda
+  database: 'railway' // Nama database Anda
 });
 
 // 4. Endpoint API (yang akan dipanggil Flutter)
@@ -22,7 +22,7 @@ app.get('/search', async (req, res) => {
   const { from, to } = req.query; 
 
   if (!from || !to) {
-    return res.status(400).json({ error: "Parameter 'from' dan 'to' wajib diisi." });
+    return res.status(400).json({ error: "Parameter 'from' dan 'to' wajib disi." });
   }
 
   // Query "Final Boss" v1.0
@@ -47,7 +47,6 @@ app.get('/search', async (req, res) => {
     const [results] = await dbPool.query(sqlQuery, [from, to]);
     
     // Kita tambahkan data "palsu" (durasi & sisaTiket)
-    // Nanti kita akan buat query SQL-nya lebih canggih
     const finalResults = results.map(train => ({
       ...train,
       durasi: "5j 30m", 
@@ -63,7 +62,6 @@ app.get('/search', async (req, res) => {
 });
 
 // 5. Jalankan Server
-// Railway akan memberi tahu kita port mana yang harus dipakai
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server API FritzLine berjalan di port ${port}`);
